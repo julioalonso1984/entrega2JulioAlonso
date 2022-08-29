f5b195bd7d0a31240f77ae18a3c9ff468d7c6f62
julioalonso1984 <julsalonso@hotmail.com>
2022-08-29 18:00:29 -0300

    master

---
+++ src/componentes/NavBars.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="barraMenu">
+                <nav className="navbar col-11 navbar-dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">ELECTRÓNICA ALONSO</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">¿Quiénes somos?</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Productos</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Contactanos</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
